66a57639b5f19149d53c4d922999f4a8e1f58f4f
dvdlgilbert <dvdlgilbert@users.noreply.github.com>
2025-11-07 19:56:46 -0600

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-full contents of tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
