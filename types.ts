75d454978400be90647ca835091b0cd561205db2
dvdlgilbert <dvdlgilbert@users.noreply.github.com>
2025-11-07 19:12:10 -0600

    types.ts

--- types.ts
+++ types.ts
@@ -1,17 +1,18 @@
+
 export interface Gig {
-  id: number; // System-generated timestamp
+  id: number;
   jobTitle: string;
-  clientName: string;
   jobDescription: string;
-  clientPhone: string;
-  clientEmail: string;
+  clientName: string;
+  clientPhone?: string;
+  clientEmail?: string;
+  clientAddress?: string;
   date: string;
-  time: string;
-  clientAddress: string;
-  jobCost: string; // Stored as string to handle currency symbols easily
-  hoursWorked: string; // Stored as string
-  jobLocation: string;
-  jobStatus: string;
+  time?: string;
+  jobLocation?: string;
+  jobCost?: number | string;
+  hoursWorked?: number | string;
+  jobStatus?: 'Planned' | 'Confirmed' | 'Completed' | 'Cancelled';
 }
 
 export interface GigPlan {
