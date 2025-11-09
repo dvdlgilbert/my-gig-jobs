3ebaac370d2fd67b57de638f0c24dafff6e25300
dvdlgilbert <dvdlgilbert@users.noreply.github.com>
2025-11-09 00:02:09 -0600

    Remove Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+full contents of tailwind.config.js
