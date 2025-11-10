e57701f3a67953d1929617fb3844f34cc901cdf2
dvdlgilbert <dvdlgilbert@users.noreply.github.com>
2025-11-10 10:52:09 -0600

    Add FilterModal component

---
+++ components/FilterModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilterModal: React.FC = () => {
+    return (
+        <div>
+            {/* Filter modal content can be added here in the future. */}
+        </div>
+    );
+};
+
+export default FilterModal;
