a14ec5895b2ae03d77a049860faf35c83ddba2b2
David L Gilbert <dvdlgilbert@users.noreply.github.com>
2026-05-08 03:34:30 -0500

    Fix module resolution error by updating App import Index.tsx
    Removed file extension from App import to fix module resolution error.

--- index.tsx
+++ index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// FIX: Fix module resolution error by adding file extension.
-import App from './App.tsx';
+// FIX: Fix module resolution error by removing file extension.
+import App from './App';
 import './index.css';
 
+console.log("APP STARTED");
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
