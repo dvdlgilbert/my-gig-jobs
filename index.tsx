de91e3ae5b2d3cd0fbbf99965bfb9f4988db4fa6
David L Gilbert <dvdlgilbert@users.noreply.github.com>
2026-05-08 03:48:31 -0500

    Correct component casing in index.tsx
    Fix casing of React and App components in render method.

--- index.tsx
+++ index.tsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-console.log("APP STARTED");
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
+  <react.strictmode>
+    <app/>
   </React.StrictMode>,
 );
+
 
