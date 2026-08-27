2a4187401dddd56e5710704e11084359b6477545
David L Gilbert <dvdlgilbert@users.noreply.github.com>
2026-08-26 20:41:45 -0500

    Configure Vite base path for deployment
    Set base path for Vite build to '/beta/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/beta/', // <-- This tells Vite to build all file links for the subfolder
   plugins: [react()],
   server: {
     host: '0.0.0.0',
