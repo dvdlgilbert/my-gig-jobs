d998ad6ff1fbb18968d196f48908280366dbedfc
David L Gilbert <dvdlgilbert@users.noreply.github.com>
2026-09-10 23:03:43 -0500

    Add service worker for caching assets

---
+++ public/serviceworker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "mygigs-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/index.css",
+  "/index.js"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
